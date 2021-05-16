3f3e2f9f55c5a1a15517926914ef7bb2fe38a363
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-16 13:38:06 -0500

    made axios post request

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ building a webframework from scratch
 
 # nodemon for auto updating and parcel creates development server
 parcel index.html
+npm run start:parcel
 
 
 # bs database to act as backend to serve and receive json
 json-server -w db.json
+npm run start:db
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
-import { User } from './models/User'
+import axios from 'axios';
 
-const user = new User({name:'Jesse', age:20});
-
-user.set({name: 'Jamie', age:30});
-// user.set({name: 'Odin'});
-// user.set({age: 45});
-
-// console.log(user.get('name'));
-// console.log(user.get('age'));
-
-user.on('change', () => {
-    console.log('Change #1');
+axios.post('http://localhost:3000/users', {
+    name: 'Jesse', 
+    age: 36
 })
 
-user.on('change', () => {
-    console.log('Change #2')
-})
 
-user.on('save', () => {
-    console.log('Save was triggered');
-})
 
-user.trigger('change');
-user.trigger('save');
-user.trigger('thisisnotanevent')
-console.log(user);
+
+
+
+
+// import { User } from './models/User'
+
+// const user = new User({name:'Jesse', age:20});
+
+// user.set({name: 'Jamie', age:30});
+// // user.set({name: 'Odin'});
+// // user.set({age: 45});
+
+// // console.log(user.get('name'));
+// // console.log(user.get('age'));
+
+// user.on('change', () => {
+//     console.log('Change #1');
+// })
+
+// user.on('change', () => {
+//     console.log('Change #2')
+// })
+
+// user.on('save', () => {
+//     console.log('Save was triggered');
+// })
+
+// user.trigger('change');
+// user.trigger('save');
+// user.trigger('thisisnotanevent')
+// console.log(user);
