4ef97e86547ebcff412a6a9593c7727a75f071c8
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-16 13:06:28 -0500

    testing event handlers method

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import { User } from './models/User'
 
 const user = new User({name:'Jesse', age:20});
 
-// user.set({name: 'Jamie', age:30});
-user.set({name: 'Odin'});
-user.set({age: 45});
+user.set({name: 'Jamie', age:30});
+// user.set({name: 'Odin'});
+// user.set({age: 45});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+// console.log(user.get('name'));
+// console.log(user.get('age'));
+
+user.on('change', () => {
+
+})
+
+console.log(user);
