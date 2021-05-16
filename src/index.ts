c52a934763128564d4a8d54ca9754f863ca497ac
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-16 09:06:09 -0500

    get and set for User created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there')
+import { User } from './models/User'
+
+const user = new User({name:'Jesse', age:20});
+
+// user.set({name: 'Jamie', age:30});
+user.set({name: 'Odin'});
+user.set({age: 45});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
