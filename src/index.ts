5aa0cbb49fc889869651185d8a2a05482525fe2e
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-16 22:41:45 -0500

    fixed 'this' binding error refactoring methods to arrow functions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { User } from './models/User';
 
 const user = new User({name: 'whatsmyname', age: 89798719283});
 
+
+
+
+
+
 // user.on('change', () => {
 //     console.log('user was changed')
 // })
